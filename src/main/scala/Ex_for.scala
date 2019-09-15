0f3ab0829b280b89a48684b1a291d8adfe502ed7
asker124143222 <124143222@qq.com>
2019-09-15 10:44:14 +0800

    hello、if、for

--- src/main/scala/Ex_for.scala
+++ src/main/scala/Ex_for.scala
@@ -6,7 +6,22 @@
   */
 object Ex_for {
   def main(args: Array[String]): Unit = {
+
+    //全闭合，包括1-10
+    println("全闭合，包括1-10")
     for (i<-1 to 10){
+      println(i)
+    }
+
+    println("\n1-10左闭右开")
+    //左闭右开
+    for(i<-1 until(10)) {
+      println(i)
+    }
+
+    println("\n左闭右开,步长为2")
+    //有步长
+    for(i<-1 until(10,2)) {
       println(i)
     }
   }
