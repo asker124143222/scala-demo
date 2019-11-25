40c27a8e63ce8e5e2706bfdce0fe1790a1dffb64
asker124143222 <124143222@qq.com>
2019-11-25 10:45:38 +0800

    update

--- src/main/java/JavaHello.java
+++ src/main/java/JavaHello.java
@@ -1,3 +1,8 @@
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @Author: xu.dm
  * @Date: 2019/9/10 11:03
@@ -5,7 +10,7 @@
  * @Description: TODO
  **/
 public class JavaHello {
-    public static void main(String args[]) throws Exception{
+    public static void main(String args[]) throws Exception {
         short s = 10;
         byte b = 10;
 
@@ -14,7 +19,7 @@ public class JavaHello {
 //        s = s +b; //错误
         //指令库里只有int类型加指令，没有short和byte的，因为指令库是char，只有255个字符（指令）
         //所以s+b涉及到s和b的类型提升，返回结果是int，上面三个语句才是错误的。
-        s = (short)( s +b); //强制把加结果的int类型转换成short类型
+        s = (short) (s + b); //强制把加结果的int类型转换成short类型
 
         long l1 = 100;
 
@@ -26,7 +31,7 @@ public class JavaHello {
         for (int i = 1; i < sLength; i = i + 2) {
             StringBuilder sb = new StringBuilder();
             StringBuilder sb2 = new StringBuilder();
-            for (int k = (sLength-i)/2; k > 0; k--) {
+            for (int k = (sLength - i) / 2; k > 0; k--) {
                 sb2.append(" ");
             }
             sb.append(sb2.toString());
@@ -36,6 +41,22 @@ public class JavaHello {
             sb.append(sb2.toString());
             System.out.println(sb.toString());
         }
+
+        //使用Instant作为时间戳代替System.currentTimeMillis
+        long getEpochSecond  = Instant.now().getEpochSecond();
+        long currentTimeMillis = System.currentTimeMillis();
+        long toEpochMilli = Instant.now().toEpochMilli();
+        System.out.println("getEpochSecond: "+getEpochSecond);
+        System.out.println("toEpochMilli: "+toEpochMilli);
+        System.out.println("System.currentTimeMillis(): "+currentTimeMillis);
+
+
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        String currentTime = localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        System.out.println("currentTime : "+currentTime);
+
 
     }
 }
